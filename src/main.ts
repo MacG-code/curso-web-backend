c8e42316f60044bbce30dfa9357448b10c1f6ea3
Mario <152038707+MacG-code@users.noreply.github.com>
2024-12-17 02:26:22 -0500

    Update main.ts V2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://curso-web-frontend-one.vercel.app'], // Usa un arreglo para los orígenes permitidos
+    origin: 'https://curso-web-frontend-one.vercel.app', // Usa un arreglo para los orígenes permitido
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     credentials: true,
   });
